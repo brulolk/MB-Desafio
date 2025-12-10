085c322d4cee3f5452ecbe5f428c71c1c95d5848
Bruno Vinicius Siqueira Santos <bruno10916@gmail.com>
2025-12-10 19:57:00 -0300

    Initial commit: Project setup

---
+++ MB-Desafio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MB-Desafio
+//
+//  Created by Bruno Vinicius on 10/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MB-DesafioUITests/MB_DesafioUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MB_DesafioUITestsLaunchTests.swift
+//  MB-DesafioUITests
+//
+//  Created by Bruno Vinicius on 10/12/25.
+//
+
+import XCTest
+
+final class MB_DesafioUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
